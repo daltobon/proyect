5b37be103ca00be1ed650f6f9b443a060eb11e24
daltobon <daniel18.tobon@gmail.com>
2018-03-15 23:50:22 -0500

    Diseñando vistas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'products', to: 'products#index'
   get 'users', to: 'users#index'
+  get 'products_user', to: 'users#product_user'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
