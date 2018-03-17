a53473b9ee6fc90fcde1177c8f14f4cbb8aa2a9e
daltobon <daniel18.tobon@gmail.com>
2018-03-16 22:15:39 -0500

    usamos resources para generar las rutas CRUD para nuestra aplicacion

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,43 @@
 class ProductsController < ApplicationController
   def index
-      @name = "Daniel"
+    @products = Product.all
+  end
+
+  def new
+  	@product = Product.new
+  end
+
+  def create
+  	@product = Product.create(products_params)
+  	redirect_to product_path(@product)
+  end
+
+  def show
+  	@product = Product.find(params[:id])
+  end
+
+  def edit
+  	@product = Product.find(params[:id])
+  	
+  end
+ 
+  def update
+  	@product = Product.find(params[:id])
+  	if @product.update(products_params)
+  		redirect_to product_path(@products), notice: "El producto fue editado"
+  	else 
+  	  render :edit, notice: "El producto no puede ser editado" 
+  	end
+  end
+
+  def delete
+  	@product = Product.find(params[:id])
+  	@product.destroy(products_params)  	
+  		redirect_to product_path(@products)
+  end
+
+  def products_params
+  	params.require(:product).permit(:name, :description, :price)
   end
 
 end
